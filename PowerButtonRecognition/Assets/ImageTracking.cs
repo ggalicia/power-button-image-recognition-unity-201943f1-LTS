2d1c33f325a736b3d3839beb8b0cff05489a5a6f
Giovani Galicia <giogalicia@gmail.com>
2020-07-20 14:59:42 -0500

    Added an AR application that recognizes and labels a 2D image of a power button

---
+++ PowerButtonRecognition/Assets/ImageTracking.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using TMPro; 
+
+public class ImageTracking : MonoBehaviour
+{
+
+    private ARTrackedImage trackedImage; 
+    private TextMeshProUGUI headerText;
+
+    void Awake()
+    {
+        trackedImage = GetComponent<ARTrackedImage>();
+        headerText = GetComponentInChildren<TextMeshProUGUI>(); 
+    }
+
+    void Start()
+    {
+        headerText.text = trackedImage.referenceImage.name; 
+    }
+}
